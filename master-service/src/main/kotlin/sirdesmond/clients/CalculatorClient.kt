d4268eb3295dcbf0c197c63852a86db4d085e92c
sirdesmond <desmond.appiah@gmail.com>
2016-11-18 08:05:40 -0600

    Added fallback for invalid requests

--- master-service/src/main/kotlin/sirdesmond/clients/CalculatorClient.kt
+++ master-service/src/main/kotlin/sirdesmond/clients/CalculatorClient.kt
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod
 /**
  * Created by kofikyei on 11/15/16.
  */
-@FeignClient("calculator")
+@FeignClient("calculator",fallback = DefaultCalculatorClient::class)
 interface CalculatorClient {
     @RequestMapping(method = arrayOf(RequestMethod.GET), value = "/optimalChange/{amount}")
     fun optimalChange(@PathVariable("amount") amount: String): Any
--- master-service/src/main/kotlin/sirdesmond/clients/DefaultCalculatorClient.kt
+++ master-service/src/main/kotlin/sirdesmond/clients/DefaultCalculatorClient.kt
@@ -1,16 +1,19 @@
 package sirdesmond.clients
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Component
-import sirdesmond.domain.Response
 
 /**
  * Created by kofikyei on 11/15/16.
- * Can be used if Hystrix fallback is enabled
  */
 
 
 
 @Component
 class DefaultCalculatorClient : CalculatorClient {
-    override fun optimalChange(amount: String): Any = Response()
+    override fun optimalChange(amount: String): Any = ObjectMapper().writeValueAsString(INVALID_AMOUNT)
+
+    companion object{
+        const val INVALID_AMOUNT = "invalid amount provided!"
+    }
 }
